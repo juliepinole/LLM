ff14c5fe1a2b8b38bc659de53b16bb30786b6104
pinolej <pinolej@gmail.com>
2024-05-28 15:52:49 +0200

    First python files to initialize the repository

---
+++ graph.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
+
---
+++ py_lib.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from collections import namedtuple
+import torch
+import torch.nn.functional as F
+from sklearn.preprocessing import LabelEncoder
+
+
